1b4caf8c25739c1645a74ce7e988006ef0947300
Gustavomalicheski <143123738+Gustavomalicheski@users.noreply.github.com>
2025-05-08 22:25:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
-  const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
-  botaoDeAcessibilidade.addEventListener('click', function() {
-    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-    opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
-  });
-  
-  const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
-  botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
-  
-  const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-  const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-  const alternaContraste = document.getElementById('alterna-contraste');
- 
-  let tamanhoAtualFonte = 1;
+    })
 
-  aumentaFonteBotao.addEventListener('click', function() {
-    tamanhoAtualFonte += 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-  });
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-  diminuiFonteBotao.addEventListener('click', function() {
-    tamanhoAtualFonte -= 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-  });
+    const alternaContraste = document.getElementById('alterna-contraste')
 
-  // Agora dentro do 'DOMContentLoaded'
-  alternaContraste.addEventListener('click', function() {
-      document.body.classList.toggle('alto-contraste');
-  });
-});
+    let tamanhoAtualFonte = 1;
 
-ScrollReveal().reveal('#inicio', { delay: 500 });
-ScrollReveal().reveal('#tropicalia', { delay: 500 });
-ScrollReveal().reveal('#galeria', { delay: 500 });
-ScrollReveal().reveal('#contato', { delay: 500 });
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+
+
+})
